4d806f65ccb39fbed490589d63aec8294a731d08
Devin Ridgway <devin.ridgway@gmail.com>
2012-12-05 15:44:00 +0000

    A few basic c

---
+++ game/GameMain.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ *
+ * @author Devin
+ */
+public class GameMain extends BasicGame{    
+    
+    public GameMain(){
+        super("24-Hour Game");
+    }
+    
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
